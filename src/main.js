653c09517bbf1718a7888626cdc40b899fe348e0
CyrusKell <cyfeye@gmail.com>
2024-04-20 02:34:47 -0400

    continued developing ui and implemented filtering plus various rendering enhancements

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 const filterMenu = document.getElementById('filter-menu');
 const startMenu = document.getElementById('start-menu');
-const filterExtraButtons = document.getElementById('filter-extra-buttons');
 const starsExtraButtons = document.getElementById('stars-extra-buttons');
+const starInfo = document.getElementById("star-info");
 
 
 document.getElementById("filter").addEventListener("click", showFilterMenu);
@@ -9,42 +9,83 @@ document.getElementById("filter-top").addEventListener("click", showFilterMenu);
 function showFilterMenu() {
     filterMenu.classList.remove('hidden'); 
     startMenu.classList.add('hidden'); 
-    filterExtraButtons.classList.remove('hidden');
     starsExtraButtons.classList.add('hidden');
+    starInfo.classList.add('hidden');
 }
 
 document.getElementById("view").addEventListener("click", showStars);
 document.getElementById("go").addEventListener("click", showStars);
-document.getElementById("back").addEventListener("click", showStars);
+document.getElementById("close-filter").addEventListener("click", showStars);
 function showStars() {
     startMenu.classList.add("hidden");
-    // filterExtraButtons.classList.remove("hidden");
-
     document.getElementById('filter-menu').classList.add('hidden'); 
-    document.getElementById('filter-extra-buttons').classList.add('hidden'); 
     document.getElementById('stars-extra-buttons').classList.remove('hidden');
+    document.getElementById("star-info").classList.remove("hidden");
 }
 
+document.getElementById("settings").addEventListener("click", showSettings);
+function showSettings() {
+    document.getElementById("settings-menu").classList.remove("hidden");
+    startMenu.classList.add('hidden'); 
+    starsExtraButtons.classList.add('hidden');
+    starInfo.classList.add('hidden');
+}
 
-function getStars() {
-    const property = document.getElementById("properties").value;
-    const greatestOrLeast = document.getElementById("greatest-or-least").value;
-    const k = document.getElementById("number").value;
+document.getElementById("close-settings").addEventListener("click", hideSettings);
+function hideSettings() {
+    document.getElementById("settings-menu").classList.add("hidden");
+    showStars();
+}
 
+const properties = document.getElementById("properties");
+const greatestOrLeast = document.getElementById("greatest-or-least");
+const greatest = document.getElementById("greatest");
+const least = document.getElementById("least");
+const number = document.getElementById("number");
+
+const constellations = document.getElementById("constellations");
+
+
+properties.addEventListener("change", updateDropdown);
+function updateDropdown() {
+    const prop = properties.value;
+    if(prop == "con") {
+        greatestOrLeast.classList.add("hidden");
+        constellations.classList.remove("hidden");
+        number.classList.add("hidden");
+    } else {
+        greatestOrLeast.classList.remove("hidden");
+        constellations.classList.add("hidden");
+        number.classList.remove("hidden");
+
+        if(prop == "dist") {
+            greatest.innerText = "Farthest";
+            least.innerText = "Closest";
+        } else if (prop == "rv") {
+            greatest.innerText = "Fastest";
+            least.innerText = "Slowest";
+        } else if (prop == "absmag") {
+            greatest.innerText = "Biggest";
+            least.innerText = "Smallest";
+        } else if (prop == "lum") {
+            greatest.innerText = "Brightest";
+            least.innerText = "Dimmest";
+        } 
+    }
 }
 
 
 //function to get array of distances
 //changeable later to include more properties
-// function getDistanceArray(jsonFile){
-//     let array = [];
-//     //for each item in jsonFile
-//     for (const key in jsonFile) {
-//         const star_info = jsonFile[key];
-//         let un_squarerooted_distance = Math.pow(star_info.x, 2)+Math.pow(star_info.y, 2)+Math.pow(star_info.z, 2);
-//         let distance = Math.sqrt(un_squarerooted_distance);
-//         //append each distance
-//         array.push(distance);
-//     }
-//     return array;
-// }
+function getDistanceArray(jsonFile){
+    let array = [];
+    //for each item in jsonFile
+    for (const key in jsonFile) {
+        const star_info = jsonFile[key];
+        let un_squarerooted_distance = Math.pow(star_info.x, 2)+Math.pow(star_info.y, 2)+Math.pow(star_info.z, 2);
+        let distance = Math.sqrt(un_squarerooted_distance);
+        //append each distance
+        array.push(distance);
+    }
+    return array;
+}
--- src/stars.js
+++ src/stars.js
@@ -4,14 +4,17 @@ import { RenderPass } from 'three/examples/jsm/Addons.js';
 import { EffectComposer } from 'three/examples/jsm/Addons.js';
 import { UnrealBloomPass } from 'three/examples/jsm/Addons.js';
 
+// DOM elements
+const loader = document.getElementById("loading-icon");
+
 let starsJson;
+let activeStars = [];
 fetch('.././data/hygdata_v41.json')
     .then((response) => response.json())
     .then((json) => {
         starsJson = json;
-        setActiveStars();
+        activeStars = starsJson;
         drawStars();
-        console.log(starsJson.length)
     })
 
 
@@ -23,7 +26,7 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000000);
 // view from earth
-camera.position.set(0, 0, 0.3)
+camera.position.set(0, 0, 0.2)
 
 // controls
 const controls = new OrbitControls( camera, canvas );
@@ -31,15 +34,13 @@ const controls = new OrbitControls( camera, canvas );
 controls.enableZoom = true;
 controls.enableDamping = true;
 
+const raycaster = new THREE.Raycaster();
+const pointer = new THREE.Vector2();
+
 // Particles
 const textureLoader = new THREE.TextureLoader();
 const particleTexture = textureLoader.load('./src/star.png'); // Add a texture to the particle
-const particlesGeometry = new THREE.BufferGeometry();
-const particlesMaterial = new THREE.PointsMaterial({
-    map: particleTexture, // Texture
-    size: 0.01, // Size of the particles
-    sizeAttenuation: true, // size of the particle will be smaller as it gets further away from the camera, and if it's closer to the camera, it will be bigger
-});
+
 
 // Bloom
 const renderScene = new RenderPass(scene, camera);
@@ -53,61 +54,116 @@ const bloomPass = new UnrealBloomPass(
 );
 composer.addPass(bloomPass);
 
-
-const activeStarsClose = [];
-const activeStarsFar = [];
-
-function setActiveStars() {
-    for(let i = 0; i < starsJson.length; i++) {
-        const star = starsJson[i];
-        // if (star.con == "Leo" ) {
-            if(star.dist <= 20) activeStarsClose.push(star);
-            else activeStarsFar.push(star);
-        // }
-    }
-}
+let renderNum = 1000;
+let activeStarsDetailed = [];
+let activeStarsLowDetail = [];
 
 function drawStars() {
-    // Draw close stars as spheres
-    for(let i = 0; i < activeStarsClose.length; i++) {
-        const star = activeStarsClose[i];
-        const color = getHex(star.ci);
-        const sphereMaterial = new THREE.MeshBasicMaterial( { color: color} ); 
-        const sphereGeometry = new THREE.SphereGeometry( star.mag / 1000, 32, 32 ); 
-        const sphere = new THREE.Mesh( sphereGeometry, sphereMaterial );
-        scene.add(sphere);
-        sphere.position.set(star.x, star.y, star.z);
+    pointer.x = -1;
+    pointer.y = -1;
+
+    activeStarsDetailed = [];
+    activeStarsLowDetail = [];
+    if(activeStars.length > renderNum) activeStars.sort(function(a, b){return a.dist - b.dist});
+    for(let i = 0; i < activeStars.length; i++) {
+        const star = activeStars[i];
+        // if(star.dist >= 99999) continue;
+        if(i <= renderNum) activeStarsDetailed.push(star);
+        else activeStarsLowDetail.push(star);
     }
 
-        // Draw far stars as particles
-        const particlesCount = activeStarsFar.length; // number of particles to be created
-        const vertices = new Float32Array(particlesCount * 3); // Float32Array is an array of 32-bit floats. This is used to represent an array of vertices. (we have 3 values for each vertex - coordinates x, y, z)
+    // Draw detailed stars as spheres
+    for(let i = 0; i < activeStarsDetailed.length; i++) {
+        const star = activeStarsDetailed[i];
+        const color = getHex(star.ci);
+        const sphereMaterial = new THREE.MeshBasicMaterial( { color: color} ); 
+        const sphereGeometry = new THREE.SphereGeometry( star.absmag / 1000, 32, 32 ); 
+        const sphere = new THREE.Mesh( sphereGeometry, sphereMaterial );
+        sphere.starData = star;
+        sphere.position.set(star.x, star.y, star.z);
+        scene.add(sphere);
+    }
 
-        for (let i = 0; i < particlesCount; i++) {
-            const star = activeStarsFar[i];
-            vertices[i * 3] = star.x;
-            vertices[i * 3  + 1] = star.y;
-            vertices[i * 3  + 2] = star.z;
-        }
+    // Draw low detail stars as particles
+    const particlesGeometry = new THREE.BufferGeometry();
+    const particlesMaterial = new THREE.PointsMaterial({    
+        map: particleTexture, // Texture
+        size: 0.01, // Size of the particles
+        sizeAttenuation: true, // size of the particle will be smaller as it gets further away from the camera, and if it's closer to the camera, it will be bigger
+    });
 
-        particlesGeometry.setAttribute(
-        'position',
-        new THREE.BufferAttribute(vertices, 3) // 3 values for each vertex (x, y, z)
-        );
-        
-        const stars = new THREE.Points(particlesGeometry, particlesMaterial);
-        scene.add(stars);
-    // }
+    const particlesCount = activeStarsLowDetail.length; // number of particles to be created
+    const vertices = new Float32Array(particlesCount * 3); // Float32Array is an array of 32-bit floats. This is used to represent an array of vertices. (we have 3 values for each vertex - coordinates x, y, z)
+
+    for (let i = 0; i < particlesCount; i++) {
+        const star = activeStarsLowDetail[i];
+        vertices[i * 3] = star.x;
+        vertices[i * 3  + 1] = star.y;
+        vertices[i * 3  + 2] = star.z;
+    }
+
+    particlesGeometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3) // 3 values for each vertex (x, y, z)
+    );
+    
+    const stars = new THREE.Points(particlesGeometry, particlesMaterial);
+    scene.add(stars);
+    hideLoader();
 }
+
+let clicked = false;
+
+// calculate pointer position
+function calculatePointer( event ) {
+	// (-1 to +1) for both components
+    clicked = true;
+	pointer.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+	pointer.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+}
+canvas.addEventListener( 'click', calculatePointer );
+
+let last;
+let lastColor;
+let lastID;
 
 
 function animate() {
     controls.update();
+
+    if(clicked) {
+        // calclate raycasting
+        // update the picking ray with the camera and pointer position
+        raycaster.setFromCamera( pointer, camera );
+
+        // calculate objects intersecting the picking ray
+        const intersects = raycaster.intersectObjects( scene.children );
+        const closest = intersects[0];
+
+        if (closest && closest.object.type != "Points") {
+            if(last) last.object.material.color.set(lastColor);
+            lastColor = closest.object.material.color.getHex();
+            if(last && closest.object == last.object) {
+                displayStarData(null);
+                last = null;
+            } else {
+                closest.object.material.color.set( 0xffffff );
+                displayStarData(closest.object.starData);
+                last = closest;
+            }
+        } else {
+            lastID = "";
+        }
+        clicked = false;
+    }
+
 	requestAnimationFrame( animate );
-	// renderer.render( scene, camera );
     composer.render();
 }
 animate();
+
+function clearScene() {
+    scene.remove.apply(scene, scene.children);
+} 
+
 
 
 // get approximate hex color value from star color index
@@ -121,8 +177,98 @@ function getHex(ci) {
     return "#ffffff";
 }
 
+document.getElementById("reset-view").addEventListener("click", resetView);
 function resetView() {
     controls.reset();
 }
 
-document.getElementById("reset-view").addEventListener("click", resetView);
+const changeRD = document.getElementById("change-rd");
+
+changeRD.addEventListener("change", changeRenderDistance)
+function changeRenderDistance() {
+    showLoader();
+    renderNum = parseInt(changeRD.value);
+    window.setTimeout(function() {
+        clearScene();
+        drawStars();
+    }, 5000)
+    
+    
+}
+
+document.getElementById("unreasonable").addEventListener("click", toggleReasonableRange);
+let reasonable = true;
+function toggleReasonableRange() {
+    if(reasonable) {
+        changeRD.setAttribute("max", "100000");
+        changeRD.setAttribute("step", "10000");
+    } else {
+        changeRD.setAttribute("max", "200");
+        changeRD.setAttribute("step", "5");
+    }
+    reasonable = !reasonable;
+    document.getElementById("reasonable-range-values").classList.toggle("hidden");
+    document.getElementById("unreasonable-range-values").classList.toggle("hidden");
+}
+
+
+function displayStarData(data) {
+    if(data == null) {
+        document.getElementById("proper-name").innerText = "";
+        document.getElementById("constellation").innerText = "";
+        document.getElementById("spectral-type").innerText = "";
+        document.getElementById("distance").innerText = "";
+        document.getElementById("radial-velocity").innerText = "";
+        document.getElementById("absolute-magnitude").innerText = "";
+        document.getElementById("luminosity").innerText = "";
+    } else {
+        document.getElementById("proper-name").innerText = data.proper;
+        document.getElementById("constellation").innerText = data.con;
+        document.getElementById("spectral-type").innerText = data.spect;
+        document.getElementById("distance").innerText = data.dist;
+        document.getElementById("radial-velocity").innerText = data.rv;
+        document.getElementById("absolute-magnitude").innerText = data.absmag;
+        document.getElementById("luminosity").innerText = data.lum;
+    }
+}
+
+
+function showLoader() {
+    loader.classList.remove("invisible");
+    window.setTimeout(function() {
+        loader.classList.remove("opacity-0");
+    }, 1000);
+
+}
+function hideLoader() {
+    loader.classList.add("opacity-0");
+    window.setTimeout(function() {
+        loader.classList.add("invisible");
+    }, 1000);
+}
+
+document.getElementById("go").addEventListener("click", filterStars);
+function filterStars() {
+    const k = parseInt(document.getElementById("number").value);
+    const property = document.getElementById("properties").value;
+    const greatestOrLeast = document.getElementById("greatest-or-least").value;
+    const constellation = document.getElementById("constellations").value;
+
+    if(property == "con") {
+        activeStars = [];
+        for(let i = 0; i < starsJson.length; i++) {
+            const star = starsJson[i];
+            if(star.con == constellation) {
+                activeStars.push(star);
+            }
+        }
+    } else {
+        starsJson.sort(function(a, b){return a[property] - b[property]});
+        if(greatestOrLeast == "least") activeStars = starsJson.slice(0, k);
+        else activeStars = starsJson.slice(-1 * k);
+    }
+
+    clearScene();
+    drawStars();
+    resetView();
+}
